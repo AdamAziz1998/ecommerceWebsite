612c013155984df145e4989673366e2e005b6a2d
AdamAziz1998 <101112712+AdamAziz1998@users.noreply.github.com>
2023-11-26 13:29:47 +0000

    message

--- src/main/java/com/eComm/store/repository/CartRepository.java
+++ src/main/java/com/eComm/store/repository/CartRepository.java
@@ -1,12 +1,15 @@
 package com.eComm.store.repository;
 
 import com.eComm.store.model.Cart;
+import com.eComm.store.model.CartItem;
+import com.eComm.store.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, UUID> {
-
+    List<CartItem> findB
 }
