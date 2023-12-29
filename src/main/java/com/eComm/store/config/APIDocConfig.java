b9c6f0305aa5b1ba905496ded116f819e0fa80b6
AdamAziz1998 <101112712+AdamAziz1998@users.noreply.github.com>
2023-12-29 10:50:45 +0000

    added the documentation library and some configurations

--- src/main/java/com/eComm/store/config/APIDocConfig.java
+++ src/main/java/com/eComm/store/config/APIDocConfig.java
@@ -3,6 +3,7 @@ package com.eComm.store.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
         title = "Ecommerce website",
         version = "0.0",
         description = "Documentation for the Ecommerce website application",
+        license = @License(name = "Apache 2.0", url = "http://foo.bar"),
         contact = @Contact(name = "Adam O'Neill", email = "adamaoneill@gmail.com")
 )
 )
